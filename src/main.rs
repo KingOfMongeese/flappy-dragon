a97713c0a277e5c6ed0c0611f47f016d0ab6f9c7
KingOfMongeese <nwohletz@gmail.com>
2025-01-21 19:04:05 -0700

    player

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,51 @@
 use bracket_lib::prelude::*;
 
+const SCREEN_WIDTH : i32 = 80;
+const SCREEN_HEIGHT : i32 = 50;
+const FRAME_DURATION : f32 = 50.0;
+
+struct Player {
+    x: i32,
+    y: i32,
+    velocity: f32,
+}
+
+impl Player {
+    fn new(x: i32, y: i32) -> Self {
+        Player {
+            x,
+            y,
+            velocity: 0.0,
+        }
+    }
+
+    fn render(&mut self, ctx: &mut BTerm) {
+        ctx.set(
+            0,
+            self.y,
+            YELLOW,
+            BLACK,
+            to_cp437('@'),
+        )
+    }
+
+    fn gravity_and_move(&mut self) {
+        if self.velocity < 2.0 {
+            self.velocity += 0.2;
+        }
+
+        self.y += self.velocity as i32;
+        self.x += 1;
+        if self.y < 0 {
+            self.y = 0;
+        }
+    }
+
+    fn flap(&mut self) {
+        self.velocity = -2.0;
+    }
+}
+
 enum GameMode {
     Menu,
     Playing,
@@ -8,28 +54,46 @@ enum GameMode {
 
 struct State {
     mode: GameMode,
+    player: Player,
+    frame_time: f32,
 }
 
 impl State {
     fn new() -> Self {
         State {
             mode: GameMode::Menu,
+            player: Player::new(5,25),
+            frame_time: 0.0,
         }
     }
 
     fn play(&mut self, ctx: &mut BTerm) {
-        // TODO: fill stub
-        println!("Playing...");
-        self.mode = GameMode::End;
+        ctx.cls_bg(NAVY);
+        self.frame_time += ctx.frame_time_ms;
+        if self.frame_time > FRAME_DURATION {
+            self.frame_time = 0.0;
+            self.player.gravity_and_move();
+        }
+
+        if let Some(VirtualKeyCode::Space) = ctx.key {
+            self.player.flap();
+        }
+
+        self.player.render(ctx);
+        ctx.print(0, 0, "Press Space to flap ><");
+
+        if self.player.y > SCREEN_HEIGHT {
+            self.mode = GameMode::End
+        }
     }
 
     fn restart(&mut self) {
-        println!("Restarting...");
+        self.player = Player::new(5, 25);
+        self.frame_time = 0.0;
         self.mode = GameMode::Playing;
     }
 
     fn main_menu(&mut self, ctx: &mut BTerm) {
-        println!("In Main Menu");
         ctx.cls();
         ctx.print_centered(5, "Your dragon awaits");
         ctx.print_centered(8, "(P) Play");
@@ -46,7 +110,6 @@ impl State {
     }
 
     fn dead(&mut self, ctx: &mut BTerm) {
-        println!("Game over");
         ctx.cls();
         ctx.print_centered(5, "GAME OVER");
         ctx.print_centered(8, "(P) Play");
